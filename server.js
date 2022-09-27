30001b04933ade375f940f18910ca1705e8bec9a
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-09-27 00:48:58 -0500

    fix the correct order in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const inputCheck = require('./utils/inputCheck');
+
 require('dotenv').config();
 let db_user = process.env.DB_USER;
 let db_pw = process.env.DB_PASSWORD;
@@ -39,8 +40,8 @@ app.get('/api/candidates', (req, res) => {
             message: 'success',
             data: rows
         });
-    })
-})
+    });
+});
 
 // GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
@@ -61,41 +62,33 @@ app.get('/api/candidate/:id', (req, res) => {
             message: 'success',
             data: row
         });
-    })
-})
+    });
+});
 
-// GET all parties
-app.get('/api/parties', (req, res) => {
-    const sql = `SELECT * FROM parties`;
+// create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
 
-    db.query(sql, (err, rows) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: rows
-        });
-    })
-})
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected, party_id)
+            VALUES (?,?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected, body.party_id];
 
-// GET a single party
-app.get('/api/party/:id', (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    
-    db.query(sql, params, (err, row) => {
+    db.query(sql, params, (err, result) => {
         if (err) {
             res.status(400).json({ error: err.message });
             return;
         }
         res.json({
-            message: `success`,
-            data: row
+            message: 'success',
+            data: body,
+            changes: result.affectedRows
         });
-    })
-})
+    });
+});
 
 // Update a candidate's party
 app.put('/api/candidate/:id', (req, res) => {
@@ -124,8 +117,8 @@ app.put('/api/candidate/:id', (req, res) => {
                 changes: result.affectedRows
             });
         }
-    })
-})
+    });
+});
 
 // Delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
@@ -146,8 +139,41 @@ app.delete('/api/candidate/:id', (req, res) => {
                 id: req.params.id
             });
         }
-    })
-})
+    });
+});
+
+// GET all parties
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// GET a single party
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: `success`,
+            data: row
+        });
+    });
+});
 
 // Delete a party
 app.delete('/api/party/:id', (req, res) => {
@@ -172,31 +198,6 @@ app.delete('/api/party/:id', (req, res) => {
     });
 });
 
-// create a candidate
-app.post('/api/candidate', ({ body }, res) => {
-    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-    if (errors) {
-        res.status(400).json({ error: errors });
-        return;
-    }
-
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-            VALUES (?,?,?)`;
-    const params = [body.first_name, body.last_name, body.industry_connected];
-
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            res.status(400).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: body
-        });
-    })
-})
-
-
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
@@ -206,3 +207,14 @@ app.use((req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
+
+/*
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
+*/
